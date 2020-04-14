56d2420ad53e654aa3e193a35f5d0ad0aae6d7fa
Tim Beals <timbeals@Tims-MacBook-Pro-2020.local>
2020-04-14 07:26:56 -0700

    -ContentView: Add elements for map view and add button

--- RideApp/RideApp/ContentView.swift
+++ RideApp/RideApp/ContentView.swift
@@ -10,7 +10,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
+            ZStack {
+                Rectangle().fill(Color.gray)
+                    .edgesIgnoringSafeArea(.all)
+                Circle().fill(Color.blue)
+                    .opacity(0.3)
+                    .frame(width: 30, height: 30)
+            }
+            
+            Button(action: {
+                
+            }, label: {
+                Image(systemName: "plus")
+            })
+                .padding()
+                .background(Color.red)
+                .foregroundColor(Color.white)
+                .font(.title)
+                .clipShape(Circle())
+                .padding(.top, -35)
+            
             Text("Distance Evaluation").font(.title).bold()
             Circle().frame(width: 300, height: 300)
             HStack {
